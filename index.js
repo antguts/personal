05984a55efebe0c62068528205a593d0899ca91f
antguts <antgut035@gmail.com>
2021-01-20 00:25:52 -0500

    added animation to about onclick

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const tl=new TimelineMax
 
+// Variables to see if checked================
+let abClicked = false
+let projClicked = false
+let resClicked = false
 
-const aboutClick=()=>{
-    alert(this);
+const aboutClick=(e)=>{
+    abClicked=true
+    tl.to('.leftAboutBorder', .3,{height: '50%', width: '50%'})
+    tl.to('.rightAboutBorder', .3,{height: '100%', width: '50%'}, '-=.3')
+    tl.to('.aboutSection h2', .3,{margin: '36vh 13vw 0 0'}, '-=.7')
+
+    changeHeaderText("About_Section")
 }
 const projClick=()=>{
     alert("projects");
@@ -9,6 +19,26 @@ const projClick=()=>{
 const resClick=()=>{
     alert("resume");
 }
+
+//GSAP Animations==================
+    const borderHover =()=>{
+        if(!abClicked){
+            tl.to('.leftAboutBorder', .3,{height: '25%', width: '25%'})
+            tl.to('.rightAboutBorder', .3,{height: '25%', width: '25%'}, '-=.3')
+        }
+    } 
+
+    const borderHoverOut =()=>{
+        if(!abClicked){
+            tl.to('.leftAboutBorder', .5,{height: '1%', width: '5%'})
+            tl.to('.rightAboutBorder', .5,{height: '1%', width: '5%'}, '-=.5')
+        }
+    } 
+
+// jquery==========================
+
+    $(".aboutSection h2").hover(borderHover,borderHoverOut)
+
 
 // gsap.registerPlugin(MotionPathPlugin);
 // Nav button hovering
@@ -28,9 +58,9 @@ const resClick=()=>{
 let changeHeaderText=(e)=>{
     document.querySelector('#location').innerHTML='' 
     // scrolls text in the header
-    let name=e.parentNode.className
-    for(let i=0;i<name.length;i++){
-        delayLetters(i,name);
+    // let name=e.parentNode.className
+    for(let i=0;i<e.length;i++){
+        delayLetters(i,e);
     }
 }
 let delayLetters=(i,name)=> { 
